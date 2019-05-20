79414aaddbf9243e79714d131080385c60f1bac6
alciancio <alcianciotto@gmail.com>
2019-05-20 18:15:19 -0500

    checkupdate added, counter fully operating. code fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,75 +5,95 @@ $(document).ready(function () {
   var keepCount = 0;
   var randomNum = 0;
 
-  function generateNum() {
-    randomNum = Math.floor((Math.random() * 101) + 19);
-    //random number between 19-120
-    $("#number-to-guess").text(randomNum);
-    console.log(randomNum);
-  }
-  generateNum();
-
-//hidden value between 1-12
+  //random number list
   var num1 = Math.floor(Math.random() * 11) + 1;
   var num2 = Math.floor(Math.random() * 11) + 1;
   var num3 = Math.floor(Math.random() * 11) + 1;
   var num4 = Math.floor(Math.random() * 11) + 1;
   console.log(num1, num2, num3, num4);
 
+  //to re-use code 
+  function generateNum() {
+    randomNum = Math.floor((Math.random() * 101) + 19);
+    //random number between 19-120
+    $("#number-to-guess").text(randomNum);
+    console.log(randomNum);
+
+    //hidden value between 1-12
+    var num1 = Math.floor(Math.random() * 11) + 1;
+    var num2 = Math.floor(Math.random() * 11) + 1;
+    var num3 = Math.floor(Math.random() * 11) + 1;
+    var num4 = Math.floor(Math.random() * 11) + 1;
+    console.log(num1, num2, num3, num4);
+
+  }
+  generateNum();
 
   $("#img1").on("click", function () {
-    $("#img1").append(num1);
+    //store the random numbers in randomnumber store values w/ num1
+    $("#img1").attr("data-randomnumber", num1);
     console.log(num1);
     keepCount = keepCount + num1;
     $("#score-box").text("Your total score is: " + keepCount);
-    
+    checkForStatus();
+    //check wins or losses
   });
 
   $("#img2").on("click", function () {
-    $("#img2").append(num2);
+    $("#img2").attr("data-randomnumber", num2);
     console.log(num2);
     keepCount = keepCount + num2;
     $("#score-box").text("Your total score is: " + keepCount);
-    
+    checkForStatus();
   });
 
   $("#img3").on("click", function () {
-    $("#img3").append(num3);
+    $("#img3").attr("data-randomnumber", num3);
     console.log(num3);
     keepCount = keepCount + num3;
     $("#score-box").text("Your total score is: " + keepCount);
-    
+    checkForStatus();
   });
 
   $("#img4").on("click", function () {
-    $("#img4").append(num4);
+    //.attr assigns values to image tag
+    $("#img4").attr("data-randomnumber", num4);
     console.log(num4);
     keepCount += parseInt(num4);
     //this doesn't work either..
     console.log(keepCount)
     $("#score-box").text("Your total score is: " + keepCount);
-    
+    checkForStatus();
   });
-  //can't seem to get keepCount to be cross-checked with randomNum. I think it has to do with it being a string and needs to be converted to an integer..but am stuck on how to program it. have tried many different ways
-
-
-  if (keepCount === randomNum) {
-    //adds one more win to counter
-    alert("You win" + wins++);
-    console.log(wins);
-    $("#wins-box").html("Wins: " + wins);
-    newGame();
-  } else if (keepCount > randomNum) {
-    alert("You lose...):" + losses++);
-    console.log(losses);
-    $("#losses-box").html("Losses: " + losses);
-    newGame();
-  };
+  
+  function checkForStatus() {
+    if (keepCount === randomNum) {
+      //adds one more win to counter
+      alert("You win!!");
+      wins++;
+      console.log(wins);
+      $("#wins-box").html("Wins: " + wins);
+      resetNum();
+      newGame();
+    } else if (keepCount > randomNum) {
+      alert("You lose...");
+      losses++;
+      console.log(losses);
+      $("#losses-box").html("Losses: " + losses);
+      resetNum();
+      newGame();
+    };
+  }
 
   function resetNum() {
-    wins = 0;
-    losses = 0;
+    $("#wins-box").html("Wins: " + wins);
+    $("#losses-box").html("Losses: " + losses);
+    //score should revert back to zero
+    $("#score-box").text("Your total score is: " + keepCount);
+    // Summation is set to zero for each image random number to sum up again
     keepCount = 0;
+    //re-display the target number
+    $("#number-to-guess").text(randomNum);
   }
 
   function newGame() {
